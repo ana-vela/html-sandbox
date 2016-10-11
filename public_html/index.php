940bbfdd092be0bb9968b6fa5818b834994a56c8
Ana Vela <avela7@cnm.edu>
2016-10-11 13:40:06 -0600

    Added image and text

--- public_html/index.php
+++ public_html/index.php
@@ -5,6 +5,23 @@
 		<title>HTML Sandbox</title>
 	</head>
 	<body>
-	<h1>Hello World!</h1>
+
+		<!-- this is a header -->
+		<header>
+			<h1>Hi! This is my page.</h1>
+		</header>
+
+		<!-- this is the main page content -->
+		<main>
+
+			<!-- begin section 1 -->
+			<section></section>
+
+			<p>hotdog systema plastic tanto concrete car refrigerator kanji physical sprawl geodesic singularity kanji. gang franchise pen cartel monofilament network tank-traps tiger-team pen wonton soup tanto motion drone. footage film drugs -ware Shibuya futurity hotdog systemic youtube Legba motion assault vinyl. sensory nodal point Chiba long-chain hydrocarbons refrigerator cyber- sensory footage neural cartel fluidity faded tanto.</p>
+				<img src="img/wizardowl.jpg" alt=" wizardowl">
+			<section></section>
+			<section></section>
+		</main>
+
 	</body>
 </html>
