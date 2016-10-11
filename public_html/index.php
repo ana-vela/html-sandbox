a4b134cd79e6c515882f8f70abe77e9c60360a50
Ana Vela <avela7@cnm.edu>
2016-10-11 13:56:38 -0600

    Added a table

--- public_html/index.php
+++ public_html/index.php
@@ -15,12 +15,48 @@
 		<main>
 
 			<!-- begin section 1 -->
-			<section></section>
+			<section>
 
 			<p>hotdog systema plastic tanto concrete car refrigerator kanji physical sprawl geodesic singularity kanji. gang franchise pen cartel monofilament network tank-traps tiger-team pen wonton soup tanto motion drone. footage film drugs -ware Shibuya futurity hotdog systemic youtube Legba motion assault vinyl. sensory nodal point Chiba long-chain hydrocarbons refrigerator cyber- sensory footage neural cartel fluidity faded tanto.</p>
 				<img src="img/wizardowl.jpg" alt=" wizardowl">
-			<section></section>
-			<section></section>
+				</section>
+
+			<!-- begin section 2 -->
+			<section>
+				<h2>My Favorite Things</h2>
+				<ul>
+					<li>Purple</li>
+					<li>Penguins</li>
+					<li>Reading</li>
+					<li>Music</li>
+				</ul>
+			</section>
+
+			<!-- begin section 3 -->
+			<section>
+				<table>
+					<tr>
+						<th>Column One</th>
+						<th>Column Two</th>
+					</tr>
+
+					<tr>
+						<td>Cat</td>
+						<td>Dog</td>
+					</tr>
+
+					<tr>
+						<td>Car</td>
+						<td>Bike</td>
+					</tr>
+
+					<tr>
+						<td>Star Wars</td>
+						<td>Wine</td>
+					</tr>
+				</table>
+
+			</section>
 		</main>
 
 	</body>
